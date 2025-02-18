4f27f931cbf88fb6fa450d4f7092b5ac4bb4bbd5
Bugra Aydingoz <bugra.aydingoz@digitalturbine.com>
2025-02-18 18:20:35 +0100

    Add list collections subnode

--- nodes/Needle/Needle.node.ts
+++ nodes/Needle/Needle.node.ts
@@ -5,15 +5,17 @@ import type {
 	INodeType,
 	INodeTypeDescription,
 } from 'n8n-workflow';
-import { Needle } from '@needle-ai/needle/v1';
 
-export class NeedleNode implements INodeType {
+import * as NeedleApi from '@needle-ai/needle/v1';
+
+export class Needle implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Needle',
 		name: 'needle',
 		icon: 'file:needle-logo-black.svg',
 		group: ['search'],
 		version: 1,
+		subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
 		description: 'Search your collections on Needle',
 		defaults: {
 			name: 'Needle',
@@ -27,11 +29,59 @@ export class NeedleNode implements INodeType {
 			},
 		],
 		properties: [
+			// Resources
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Collection',
+						value: 'collection',
+					},
+				],
+				default: 'collection',
+			},
+
+			// Operations
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['collection'],
+					},
+				},
+				options: [
+					{
+						name: 'List',
+						value: 'list',
+						action: 'List collections',
+					},
+					{
+						name: 'Search',
+						value: 'search',
+						action: 'Search a collection',
+					},
+				],
+				default: 'search',
+			},
+
+			// Fields
 			{
 				displayName: 'Collection ID',
 				name: 'collection_id',
 				type: 'string',
 				default: '',
+				displayOptions: {
+					show: {
+						operation: ['search'],
+						resource: ['collection'],
+					},
+				},
 			},
 			{
 				displayName: 'Prompt',
@@ -41,29 +91,36 @@ export class NeedleNode implements INodeType {
 				description: 'Prompt to ask Needle',
 				placeholder: 'Ask Needle...',
 				required: true,
+				displayOptions: {
+					show: {
+						operation: ['search'],
+						resource: ['collection'],
+					},
+				},
 			},
 		],
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const resource = this.getNodeParameter('resource', 0);
+		const operation = this.getNodeParameter('operation', 0);
+
+		console.log({ resource, operation });
+
 		const items = this.getInputData();
 		const responseData: INodeExecutionData[] = [];
 
 		for (let i = 0; i < items.length; i++) {
 			try {
-				const collection_id = this.getNodeParameter('collection_id', i) as string;
-				const prompt = this.getNodeParameter('prompt', i) as string;
-
-				const credentials = await this.getCredentials('api_key', i);
-				const apiKey = credentials['api_key'] as string;
-				if (!apiKey) {
-					throw Error('API Key is required');
+				switch (resource) {
+					case 'collection':
+						switch (operation) {
+							case 'list':
+								await listCollections(this, i);
+							case 'search':
+								await searchCollection(this, i);
+						}
 				}
-
-				const ndl = new Needle({ apiKey });
-
-				const results = await ndl.collections.search({ collection_id, text: prompt });
-				results.forEach((r) => responseData.push({ json: r, index: i }));
 			} catch (error) {
 				if (this.continueOnFail()) {
 					responseData.push({
@@ -77,6 +134,40 @@ export class NeedleNode implements INodeType {
 			}
 		}
 
-		return [items];
+		return [responseData];
 	}
+}
+
+async function listCollections(
+	functions: IExecuteFunctions,
+	index: number,
+): Promise<INodeExecutionData[]> {
+	const responseData: INodeExecutionData[] = [];
+	const credentials = await functions.getCredentials('needleApi', index);
+	const apiKey = credentials['api_key'] as string;
+	if (!apiKey) {
+		throw Error('API Key is required');
+	}
+	const ndl = new NeedleApi.Needle({ apiKey });
+	const results = await ndl.collections.list();
+	results.forEach((r) => responseData.push({ json: r, index: index }));
+	return responseData;
+}
+
+async function searchCollection(
+	functions: IExecuteFunctions,
+	index: number,
+): Promise<INodeExecutionData[]> {
+	const responseData: INodeExecutionData[] = [];
+	const collection_id = functions.getNodeParameter('collection_id', index) as string;
+	const prompt = functions.getNodeParameter('prompt', index) as string;
+	const credentials = await functions.getCredentials('needleApi', index);
+	const apiKey = credentials['api_key'] as string;
+	if (!apiKey) {
+		throw Error('API Key is required');
+	}
+	const ndl = new NeedleApi.Needle({ apiKey });
+	const results = await ndl.collections.search({ collection_id, text: prompt });
+	results.forEach((r) => responseData.push({ json: r, index: index }));
+	return responseData;
 }
